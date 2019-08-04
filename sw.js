2c9fe36a38ee49b7ddac4337a74c2ebfd60b0062
austencm <austencm@gmail.com>
2019-08-04 13:32:22 -0700

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-87a0b7d98430e8560507.js"
+    "url": "webpack-runtime-38eaeed13820b8ab8696.js"
   },
   {
     "url": "app.329fbcee42070355937f.css"
@@ -39,7 +39,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "cbbb6244fff021c6ced885d22d73c3e3"
+    "revision": "107b58f699e3535be60559b48b53ca89"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"
